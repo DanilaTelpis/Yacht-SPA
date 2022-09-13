529999d9077b8729989ec60d7b0aced83b11b2e3
Boris <b0risk4rabut@gmail.com>
2022-09-13 20:40:17 +0300

    Вторая версия всё ещё сырая альфа

--- Project/main/scripts/script.js
+++ Project/main/scripts/script.js
@@ -2,11 +2,11 @@
 document.getElementById('hamburger-icon').onclick = function(){
   document.getElementById('hamburger-menu').classList.remove('closed');
   document.getElementById('hamburger-menu').classList.add('open');
-  document.querySelector('#hamburger-menu > i').style.display='block';
+  document.querySelector('#hamburger-menu > i').style.right='10px';
 }
 
 document.getElementById('x-mark').onclick = function(event){
   event.target.parentNode.classList.remove('open');
   event.target.parentNode.classList.add('closed');
-  document.querySelector('#hamburger-menu > i').style.display='none';
+  document.querySelector('#hamburger-menu > i').style.right='-20px';
 }
