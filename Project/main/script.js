3ce1afa365180c34d1dccae5f312d48b1f79f383
Boris Karabut <93115460+RoastedPikachu@users.noreply.github.com>
2022-09-17 11:29:01 +0300

    Add files via upload

---
+++ Project/main/script.js
@@ -0,0 +1,15 @@
+
+document.getElementById('hamburger-icon').onclick = function(){
+  document.getElementById('hamburger-menu').classList.remove('closed');
+  document.getElementById('hamburger-menu').classList.add('open');
+  document.querySelector('#hamburger-menu > i').style.right='10px';
+}
+
+document.getElementById('x-mark').onclick = function(event){
+  event.target.parentNode.classList.remove('open');
+  event.target.parentNode.classList.add('closed');
+  document.querySelector('#hamburger-menu > i').style.right='-20px';
+}
+
+
+
